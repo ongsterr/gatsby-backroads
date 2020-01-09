834f1333871b486b9a8be633ec4492cb47cf4860
Chris Ong <ong.chris11@gmail.com>
2020-01-09 22:25:02 +1100

    feat: add homepage banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,21 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 import Layout from '../components/Layout'
+import SimpleHero from '../components/SimpleHero'
+import Banner from '../components/Banner'
 
-export default () => <Layout>Hello world!</Layout>
+export default () => (
+  <Layout>
+    <SimpleHero>
+      <Banner
+        title="Continue Exploring"
+        info="Lorem ipsum dolor sit amet consectetur adipisicing elit. "
+      >
+        <Link to="/tours" className="btn-white">
+          Explore Tours
+        </Link>
+      </Banner>
+    </SimpleHero>
+  </Layout>
+)
