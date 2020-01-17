8f7a688ef2de6396050351d0d27fe4df9b56ee22
Chris Ong <ong.chris11@gmail.com>
2020-01-17 23:01:58 +1100

    chore: add dotenv and update space id

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,7 +42,7 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: process.env.SPACE_ID,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
         // Learn about environment variables: https://gatsby.dev/env-vars
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
