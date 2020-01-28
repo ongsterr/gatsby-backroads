cc11af34fd2d3bd76238438b6077d9c8c10a00ba
Chris Ong <ong.chris11@gmail.com>
2020-01-28 11:11:51 +1100

    chore: comment out blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,32 +1,32 @@
-import React from 'react'
-import { graphql } from 'gatsby'
+// import React from 'react'
+// import { graphql } from 'gatsby'
 
-import Layout from '../components/Layout'
-// import Images from '../examples/Images'
-import StyledHero from '../components/StyledHero'
-import BlogList from '../components/Blog/BlogList'
+// import Layout from '../components/Layout'
+// // import Images from '../examples/Images'
+// import StyledHero from '../components/StyledHero'
+// import BlogList from '../components/Blog/BlogList'
 
-const blog = ({ data }) => {
-  const { blogBcg } = data
+// const blog = ({ data }) => {
+//   const { blogBcg } = data
 
-  return (
-    <Layout>
-      <StyledHero img={blogBcg.childImageSharp.fluid} />
-      <BlogList />
-    </Layout>
-  )
-}
+//   return (
+//     <Layout>
+//       <StyledHero img={blogBcg.childImageSharp.fluid} />
+//       <BlogList />
+//     </Layout>
+//   )
+// }
 
-export const query = graphql`
-  query {
-    blogBcg: file(relativePath: { eq: "blogBcg.jpeg" }) {
-      childImageSharp {
-        fluid(maxWidth: 4160, quality: 90) {
-          ...GatsbyImageSharpFluid_withWebp
-        }
-      }
-    }
-  }
-`
+// export const query = graphql`
+//   query {
+//     blogBcg: file(relativePath: { eq: "blogBcg.jpeg" }) {
+//       childImageSharp {
+//         fluid(maxWidth: 4160, quality: 90) {
+//           ...GatsbyImageSharpFluid_withWebp
+//         }
+//       }
+//     }
+//   }
+// `
 
-export default blog
+// export default blog
